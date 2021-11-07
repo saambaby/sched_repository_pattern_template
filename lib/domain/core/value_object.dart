285685bded70b22b653796ba57e722aaa370e438
SaamBaby <sambaby870@gmail.com>
2021-11-07 04:43:01 -0500

    Firebase Auth Logic Completed

--- lib/domain/core/value_object.dart
+++ lib/domain/core/value_object.dart
@@ -3,11 +3,18 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:sched/domain/auth/value_objects.dart';
 import 'package:sched/domain/core/failures/failures.dart';
 
+import 'errors.dart';
+
 
 @immutable
 abstract class ValueObjects<T> {
 
   Either<ValueFailure<T>, T>get value;
+  /// Throws [UnexpectedValueError] containing  [ValueFailure]
+  T getOrCrash(){
+    return value.fold((left) => throw UnexpectedValueError(left),
+            (right) => right);
+  }
   bool isValid()=> value.isRight();
   @override
   bool operator ==(Object other) {
